033c6ba3a1feb4092471cd88b7d97ecfda5f6d70
Gaurav Mukati <gaurav.mukati99@gmail.com>
2023-12-20 14:07:23 +0530

    Admin API

--- src/main/java/in/ies_application/ADMIN_API/repos/PlanRepo.java
+++ src/main/java/in/ies_application/ADMIN_API/repos/PlanRepo.java
@@ -1,10 +1,15 @@
 package in.ies_application.ADMIN_API.repos;
 
+import feign.Param;
 import in.ies_application.ADMIN_API.entity.PlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlanRepo extends JpaRepository<PlanEntity, Integer> {
+
+    @Query("update PlanEntity set activeSw = :status where planId = :planId")
+    public Integer updatePlanStatus(@Param("planId") Integer planId, @Param("status") String status);
 
 }
--- src/main/java/in/ies_application/ADMIN_API/services/PlanServiceImpl.java
+++ src/main/java/in/ies_application/ADMIN_API/services/PlanServiceImpl.java
@@ -1,29 +1,65 @@
 package in.ies_application.ADMIN_API.services;
 
 import in.ies_application.ADMIN_API.bindings.PlanForm;
+import in.ies_application.ADMIN_API.entity.PlanEntity;
+import in.ies_application.ADMIN_API.repos.PlanRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlanServiceImpl implements PlanService {
+
+    @Autowired
+    private PlanRepo planRepo;
     @Override
     public boolean createPlan(PlanForm form) {
-        return false;
+        PlanEntity planEntity = new PlanEntity();
+        BeanUtils.copyProperties(form, planEntity);
+        planEntity.setActiveSw("Y");
+        planRepo.save(planEntity);
+
+        return true;
     }
 
     @Override
     public List<PlanForm> viewAllPlans() {
-        return null;
+
+        List<PlanEntity> entities = planRepo.findAll();
+        List<PlanForm> plans = new ArrayList<>();
+
+        for(PlanEntity entity: entities){
+            PlanForm plan = new PlanForm();
+            BeanUtils.copyProperties(entity, plan);
+            plans.add(plan);
+        }
+
+        return plans;
     }
 
     @Override
     public PlanForm getPlanById(Integer planId) {
+        Optional<PlanEntity> optional = planRepo.findById(planId);
+        if(optional.isPresent()){
+            PlanForm plan = new PlanForm();
+            BeanUtils.copyProperties(optional,plan);
+            return plan;
+        }
         return null;
     }
 
     @Override
     public String changePlanStatus(Integer planId, String status) {
-        return null;
+        int cnt = planRepo.updatePlanStatus(planId, status);
+
+        if(cnt > 0){
+            return "Status Changed";
+        }
+        return "Failed to change Status";
     }
 }
